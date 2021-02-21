32cf7579bf2fa809226ce0382ff403816b30cd8a
viidak <vidakbussines@hotmail.com>
2021-02-21 13:04:40 +0100

    Some more work on route finding

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -42,20 +42,26 @@ class RouteController extends Controller
     public function findRoutes(Request $request)
     {
         $request->validate([
-            'city_from' => 'string|max:255|required|regex:/^[a-zA-Z]+$/u',
-            'city_to' => 'string|max:255|required|regex:/^[a-zA-Z]+$/u',
+            // 'city_from' => 'string|max:255|required|regex:/^[a-zA-Z]+$/u',
+            // 'city_to' => 'string|max:255|required|regex:/^[a-zA-Z]+$/u',
+            'city_to_id' => 'required|numeric|min:1',
+            'city_from_id' => 'required|numeric|min:1',
         ]);
 
         $results = [];
 
         
         // Check if there are Airports in given cities
-        $column = 'city';
-        $cityA = $request->city_from;
-        $cityB = $request->city_to;
+        // $column = 'city';
+        // $cityA = $request->city_from;
+        // $cityB = $request->city_to;
 
-        $startAirports = Airport::where($column, $cityA)->get();
-        $endAirports = Airport::where($column, $cityB)->get();
+        // $startAirports = Airport::where($column, $cityA)->get();
+        // $endAirports = Airport::where($column, $cityB)->get();
+
+        $column = 'airport_id';
+        $startAirports = Airport::where($column, $request->city_from_id)->get();
+        $endAirports = Airport::where($column, $request->city_to_id)->get();
         
         if (!$startAirports || !$endAirports)
         {
@@ -70,11 +76,15 @@ class RouteController extends Controller
         $endRoutesList = [];
         foreach ($endAirports as $endAirport)
         {
-            $endRoutesList[] = Route::where('source_airport_id', $endAirport->airport_id)->get();
+            $endRoutesList[] = Route::where('destination_airport_id', $endAirport->airport_id)->get();
         }
 
         $allRoutes = Route::all();
-        $dirrectRoutes = [];
+        $allRoutes = $allRoutes->keyBy('route_id');
+
+        $routes = [];
+        $second = [];
+        $remainingRoutes = [];
         foreach ($startRoutesList as $startRoutes)
         {
             foreach ($startRoutes as $startRoute)
@@ -83,13 +93,59 @@ class RouteController extends Controller
                 {
                     foreach ($endRoutes as $endRoute)
                     {
-                        $dirrectRoutes[] = $allRoutes->filter(function($route) {
-
+                        // First round
+                        $foundRoutes = $allRoutes->filter(function($route) use($startRoute, $endRoute) {
+                            if ($route->source_airport_id == $startRoute->source_airport_id
+                                && $route->destination_airport_id == $endRoute->destination_airport_id)
+                            {
+                                return $route;
+                            }
                         });
+                        array_push ($routes, $foundRoutes);
+
+                        foreach ($foundRoutes as $found) {
+                            $allRoutes->forget($found->route_id);
+                        }
+
+                        // Next start and end airports
+                        $nextStart = [];
+                        $nextEnd = [];
+                        if ($startRoute->destination_airport_id !== $endRoute->destination_airport_id) {
+                            $nextStart = $allRoutes->filter(function($route) use($startRoute) {
+                                if ($route->source_airport_id == $startRoute->destination_airport_id)
+                                {
+                                    return $route;
+                                }
+                            });
+                        }
+
+                        if ($startRoute->source_airport_id !== $endRoute->source_airport_id) {
+                            $nextEnd = $allRoutes->filter(function($route) use($endRoute) {
+                                if ($route->destination_airport_id == $endRoute->source_airport_id)
+                                {
+                                    return $route;
+                                }
+                            });
+                        }
+
+                        // Second Round
+                        $foundSecond = $allRoutes->filter(function($route) use($startRoute, $endRoute) {
+                            if ($route->source_airport_id == $startRoute->source_airport_id
+                                && $route->destination_airport_id == $endRoute->destination_airport_id)
+                            {
+                                return $route;
+                            }
+                        });
+
+                        foreach ($foundSecond as $found) {
+                            $allRoutes->forget($found->route_id);
+                        }
+                        array_push ($second, $foundSecond);
                     }
                 }
             }
         }
+        dd($routes, $second);
 
         return response()->json($results, 200);
     }
